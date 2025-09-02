cfc742d0eb9d658356e3506d37fbf012d874814d
wilkerfran <wilkerfran@gmail.com>
2025-09-02 15:20:19 -0300

    Feature: Appointment - CRUD e verificação de conflito

--- src/main/java/com/barbershop/scheduler/repository/AppointmentRepository.java
+++ src/main/java/com/barbershop/scheduler/repository/AppointmentRepository.java
@@ -2,6 +2,8 @@ package com.barbershop.scheduler.repository;
 
 import com.barbershop.scheduler.model.Appointment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.time.LocalDateTime;
 
 public interface AppointmentRepository extends JpaRepository<Appointment, Long> {
+    boolean existsByEmployee_IdAndDateTime(Long employeeId, LocalDateTime dateTime);
 }
